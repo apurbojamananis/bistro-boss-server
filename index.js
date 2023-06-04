e026ffac907c5995c25f73a86d70b6d3b0425d70
apurbojamananis <apurbojaman.anis@gmail.com>
2023-06-04 23:26:08 +0600

    added users api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orqgdcn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,10 +23,31 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  const usersCollection = client.db("bistroDb").collection("users");
   const menuCollection = client.db("bistroDb").collection("menu");
   const reviewCollection = client.db("bistroDb").collection("reviews");
   const cartCollection = client.db("bistroDb").collection("carts");
 
+  // users
+
+  app.get("/users", async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+  });
+
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+
+    const query = { email: user.email };
+    const existingUser = await usersCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: "user already exists" });
+    }
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
+
+  // Menu
   app.get("/menu", async (req, res) => {
     const result = await menuCollection.find().toArray();
     res.send(result);
@@ -51,11 +72,16 @@ async function run() {
 
   app.post("/carts", async (req, res) => {
     const item = req.body;
-    console.log(item);
     const result = await cartCollection.insertOne(item);
     res.send(result);
   });
 
+  app.delete("/carts/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  });
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
